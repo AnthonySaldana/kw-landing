201b0b1b2605ac6b45e9a9a5ef4201e1635d7ee9
Anthony Saldana <anthony.saldana@motortrend.com>
2019-04-22 13:53:31 -0700

    Updates

---
+++ precache-manifest.6a71c452e51cd6604bb8eb87ec21e266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fab7386ace9876ee2f09709d39cc3b0",
+    "url": "/kw-landing/static/media/footer-logo.2fab7386.svg"
+  },
+  {
+    "revision": "2bd9a1a86311e02aecc5",
+    "url": "/kw-landing/static/js/runtime~main.b59a6eb5.js"
+  },
+  {
+    "revision": "7887b9f63931366b0003",
+    "url": "/kw-landing/static/js/main.13984795.chunk.js"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/kw-landing/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "7887b9f63931366b0003",
+    "url": "/kw-landing/static/css/main.70e81c06.chunk.css"
+  },
+  {
+    "revision": "8f2efddc2195aba2ad3474ab9aeaafa3",
+    "url": "/kw-landing/index.html"
+  }
+];
