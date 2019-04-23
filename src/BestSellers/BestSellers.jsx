f76e37643d69901a277fc0ba62d3bed6643d958a
Anthony Saldana <anthony.saldana@motortrend.com>
2019-04-22 22:49:27 -0700

    add react breakpoints. set 100% center slider percentage on mobile

--- src/BestSellers/BestSellers.jsx
+++ src/BestSellers/BestSellers.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import { Carousel } from 'react-responsive-carousel';
+import { withBreakpoints } from 'react-breakpoints'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './bestsellers.scss'
 
 class BestSellers extends Component {
   render() {
-    const { products, header, subheader } = this.props
+    const { products, header, subheader, currentBreakpoint } = this.props
     let productKey = 0
     return (
       <div className="slide-container">
         <h2 className="header" dangerouslySetInnerHTML={{ __html: header }}></h2>
         <p className="subhheader" dangerouslySetInnerHTML={{ __html: subheader }}></p>
-        <Carousel showArrows={true} showThumbs={false} centerMode={true} centerSlidePercentage={23} infiniteLoop={true} swipeable={true}
+        <Carousel showArrows={true} showThumbs={false} centerMode={true} centerSlidePercentage={ currentBreakpoint === "desktop" ? 23 : 100 } infiniteLoop={true} swipeable={true}
         statusFormatter={(current, total) => {return(`${current}/${total}`)}}>
           { products.map((product) => {
               productKey += 1
@@ -28,4 +29,4 @@ class BestSellers extends Component {
   }
 }
 
-export default BestSellers
+export default withBreakpoints( BestSellers )
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactBreakpoints from 'react-breakpoints'
+const breakpoints = {
+    mobile: 425,
+    tabelt: 768,
+    desktop: 769
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( 
+<ReactBreakpoints breakpoints={breakpoints}>
+    <App />
+</ReactBreakpoints>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
