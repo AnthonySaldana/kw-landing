5f066238237e041b321e6b303a7d97157ec00a9b
Anthony Saldana <anthony.saldana@motortrend.com>
2019-04-27 11:02:02 -0700

    Fix some bugs - desktop + mobile

--- src/CTA/CTA.jsx
+++ src/CTA/CTA.jsx
@@ -4,13 +4,16 @@ import './cta.scss'
 
 class CTA extends Component {
   render() {
-    const { header, subheader, content, image, mobileimage, flip, btnlink } = this.props
+    const { header, subheader, content, image, mobileimage, flip, btnlink, offset } = this.props
+    let wrapClasses = "ctawrapper grid"
+    if ( offset ) { wrapClasses += " offset" }
+
     return (
-      <div className="ctawrapper grid" >
+      <div className={wrapClasses}>
       { flip ? <React.Fragment>
           <div className="col-half col-img sm-hide" style={{ backgroundImage: `url(${image})` }}></div>
           <div className="col-half col-img lg-hide" style={{ backgroundImage: `url(${mobileimage})` }}></div>
-          <div className="col-half">
+          <div className="col-half col-content">
             <div className="ctacontentwrapper">
               <h1 dangerouslySetInnerHTML={{ __html: header }}></h1>
               { subheader ? <h3 className="subheader" dangerouslySetInnerHTML={{ __html: subheader }}></h3> : null }
@@ -19,7 +22,7 @@ class CTA extends Component {
             </div>
         </div>        
       </React.Fragment> : <React.Fragment>
-        <div className="col-half">
+        <div className="col-half col-content">
           <div className="ctacontentwrapper">
             <h1 dangerouslySetInnerHTML={{ __html: header }}></h1>
             { subheader ? <h3 className="subheader" dangerouslySetInnerHTML={{ __html: subheader }}></h3> : null }
