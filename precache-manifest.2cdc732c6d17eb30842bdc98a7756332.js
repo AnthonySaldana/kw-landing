b5dfe641c8b4eb2c8ddd2e9e1653687a43e613a5
Anthony Saldana <anthony.saldana@motortrend.com>
2019-04-27 23:44:59 -0700

    Updates

--- precache-manifest.2cdc732c6d17eb30842bdc98a7756332.js
+++ precache-manifest.2cdc732c6d17eb30842bdc98a7756332.js
@@ -20,23 +20,23 @@ self.__precacheManifest = [
     "url": "/kw-landing/static/js/runtime~main.b59a6eb5.js"
   },
   {
-    "revision": "57749759cfe31b6c1d42",
-    "url": "/kw-landing/static/js/main.1dfd7400.chunk.js"
+    "revision": "5af85ccb5caa5cdea35f",
+    "url": "/kw-landing/static/js/main.547b861b.chunk.js"
   },
   {
     "revision": "68a053919bed637481a2",
     "url": "/kw-landing/static/js/2.235f9a6d.chunk.js"
   },
   {
-    "revision": "57749759cfe31b6c1d42",
-    "url": "/kw-landing/static/css/main.c4450f73.chunk.css"
+    "revision": "5af85ccb5caa5cdea35f",
+    "url": "/kw-landing/static/css/main.59d9746e.chunk.css"
   },
   {
     "revision": "68a053919bed637481a2",
     "url": "/kw-landing/static/css/2.4569a16f.chunk.css"
   },
   {
-    "revision": "a40989020e5daf1fe8170d5b7402d685",
+    "revision": "9d73217ffe50b9eaaea23cd368f4b04b",
     "url": "/kw-landing/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kw-landing/precache-manifest.002cc862cf6863ead912e31eab9142f6.js"
+  "/kw-landing/precache-manifest.2cdc732c6d17eb30842bdc98a7756332.js"
 );
 
 workbox.clientsClaim();
